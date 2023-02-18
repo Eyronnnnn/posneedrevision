4ffb0bed5cae016f4722ccf4fe81aa99e9c63d0b
Eyronnnnn <125238804+Eyronnnnn@users.noreply.github.com>
2023-02-18 01:14:25 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/png" href="C:\xamppL\htdocs\pos\Restro\admin\assets\img\iconsfavicon-32x32.png">
+
+
+    <title>MT Mango POS System</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #FFA500;
+            color: #0B0705;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 120vh;
+            margin: 2;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #000302;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+    <div class="flex-center full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                MT MANGO POS
+            </div>
+
+            <div class="links">
+                <a href="Restro/admin/">Admin Log In</a>
+                <a href="Restro/cashier/">Cashier Log In</a>
+            </div>
+        </div>
+        <br>
+        <div class="links">
+                <a href="Restro/admin/">About Us</a>
+    </div>
+</br>
+<div class="links">
+                <a href="Restro/admin/">Contact</a>
+    </div>
+<br>
+<div class="links">
+                <a href="Restro/admin/">Location</a>
+    </div>
+<div class="links">
+                <a href="Restro/admin/">Message US</a>
+    </div>
+</body>
+</html>
